91b6df7ddd6739d7bfb30ed00d0c33b1886b2ab0
Vivian Eze <ezevive@gmail.com>
2026-02-05 17:59:40 +0100

    Refactor ReflectingActivity for clarity and updates

--- week05/Mindfulness/ReflectingActivity.cs
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -1,47 +1,45 @@
 class ReflectingActivity : Activity
 {
-    private List<string> _prompts = new List<string>()
+    private List<string> _prompts = new()
     {
-        "Think of a time you helped someone.",
-        "Think of a time you did something difficult.",
-        "Think of a time you showed courage.",
-        "Think of a time you overcame a challenge."
+        "Think of a time when you helped someone.",
+        "Think of a time when you did something difficult.",
+        "Think of a time when you showed courage.",
+        "Think of a time when you overcame a challenge."
     };
 
-    private List<string> _questions = new List<string>()
+    private List<string> _questions = new()
     {
-        "Why was this meaningful?",
+        "Why was this experience meaningful?",
         "How did you feel?",
         "What did you learn?",
-        "How can you apply this again?",
-        "What made this successful?"
+        "How can you apply this experience in the future?",
+        "What made this time successful?"
     };
 
-    private Random _rand = new Random();
+    private Random _random = new();
 
     public ReflectingActivity()
         : base("Reflecting Activity",
-        "Reflect on times you showed strength and resilience.")
+        "This activity will help you reflect on times you showed strength.")
     { }
 
     public override void Run()
     {
         DisplayStartingMessage();
-
         DisplayPrompt();
         DisplayQuestions();
-
         DisplayEndingMessage();
     }
 
     public string GetRandomPrompt()
     {
-        return _prompts[_rand.Next(_prompts.Count)];
+        return _prompts[_random.Next(_prompts.Count)];
     }
 
     public string GetRandomQuestion()
     {
-        return _questions[_rand.Next(_questions.Count)];
+        return _questions[_random.Next(_questions.Count)];
     }
 
     public void DisplayPrompt()
@@ -53,9 +51,9 @@ class ReflectingActivity : Activity
 
     public void DisplayQuestions()
     {
-        DateTime end = DateTime.Now.AddSeconds(_duration);
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
 
-        while (DateTime.Now < end)
+        while (DateTime.Now < endTime)
         {
             Console.Write($"\n> {GetRandomQuestion()} ");
             ShowSpinner(4);
